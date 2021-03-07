0a098414877f524da901ecc034f61b8d6a7546c5
Bartosz Król <barto.kro@gmail.com>
2021-03-07 22:11:36 +0100

    create checkPlayerLength function, add removeEventListener to clicked box

--- script.js
+++ script.js
@@ -9,16 +9,24 @@ const board = [
 const player1 = [];
 const player2 = [];
 
-boxes.forEach((box) =>
-	box.addEventListener("click", (e) => {
-		if (player1.length === player2.length) {
-			player1.push(e);
-			e.target.textContent = "X";
-			board[e.target.dataset.row][e.target.dataset.column] = e.target;
-		} else {
-			player2.push(e);
-			e.target.textContent = "O";
-			board[e.target.dataset.row][e.target.dataset.column] = e.target;
-		}
-	})
-);
+const checkPlayerLength = (e) => {
+	board[e.target.dataset.row][e.target.dataset.column] = e.target;
+	e.target.removeEventListener("click", checkPlayerLength);
+
+	if (player1.length === player2.length) {
+		player1.push(e);
+		e.target.textContent = "X";
+	} else {
+		player2.push(e);
+		e.target.textContent = "O";
+	}
+};
+
+boxes.forEach((box) => {
+	box.addEventListener("click", checkPlayerLength);
+	// box.forEach((box) => {
+	// 	if (box.textContent !== "") {
+	// 		box.removeEventListener("click", checkPlayerLength);
+	// 	}
+	// });
+});
