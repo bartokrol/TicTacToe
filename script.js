affe646f34fda5429cb9fd5c56bf0c79d6ddeac3
Bartosz Król <barto.kro@gmail.com>
2021-03-09 19:23:21 +0100

    edit classes in index.html in style.css and add eventListeners to each box, and add mouseOverElement,  mouseOutElement functions in script.js

--- script.js
+++ script.js
@@ -1,60 +1,92 @@
 const boxes = Array.from(document.querySelectorAll(".board--box"));
 
-const board = [
-	[, ,],
-	[, ,],
-	[, ,],
-];
+const markChoice = document.querySelector(".mark--choice");
 
-const winningCombinations = [
-	[1, 2, 3],
-	[4, 5, 6],
-	[7, 8, 9],
-	[1, 4, 7],
-	[2, 5, 8],
-	[3, 6, 9],
-	[1, 5, 9],
-	[3, 5, 7],
-];
+const buttons = [...document.querySelectorAll("button")];
+buttons.forEach((el) => {
+	el.addEventListener("click", (e) => {
+		player1.mark = e.target.value;
+		markChoice.classList.add("inactive");
+	});
+});
 
-const player1 = [];
+const player1 = {
+	mark: null,
+};
+
+// const board = [
+// 	[, ,],
+// 	[, ,],
+// 	[, ,],
+// ];
+
+// const winningCombinations = [
+// 	[1, 2, 3],
+// 	[4, 5, 6],
+// 	[7, 8, 9],
+// 	[1, 4, 7],
+// 	[2, 5, 8],
+// 	[3, 6, 9],
+// 	[1, 5, 9],
+// 	[3, 5, 7],
+// ];
+
+// const player1 = [];
 const player2 = [];
 
-const checkPlayerLength = (e) => {
+// const checkPlayerLength = (e) => {
+// 	if (player1.length === player2.length) {
+// 		player1.push(Number(e.target.id));
+// 		e.target.classList.add("cross");
+// 	} else {
+// 		player2.push(Number(e.target.id));
+// 		e.target.classList.add("circle");
+// 	}
+// 	board[e.target.dataset.row][e.target.dataset.column] = e.target.className;
+// 	e.target.removeEventListener("click", checkPlayerLength);
+// 	if (player1.length > 2 || player2.length > 2) {
+// 		checkWinner();
+// 	}
+// };
+
+// const checkWinner = () => {
+// 	for (let combination of winningCombinations) {
+// 		if (combination.every((el) => player1.includes(el))) {
+// 			console.log("gracz pierwszy wygrał!");
+// 			boxes.forEach((box) => {
+// 				box.removeEventListener("click", checkPlayerLength);
+// 			});
+// 		} else if (combination.every((el) => player2.includes(el))) {
+// 			console.log("gracz drugi wygrał!");
+// 			boxes.forEach((box) => {
+// 				box.removeEventListener("click", checkPlayerLength);
+// 			});
+// 		}
+// 	}
+// };
+
+// // const checkDraw = () => {
+// // 	if()
+// // }
+
+const mouseOverElement = (e) => {
 	if (player1.length === player2.length) {
-		player1.push(Number(e.target.id));
 		e.target.classList.add("cross");
 	} else {
-		player2.push(Number(e.target.id));
 		e.target.classList.add("circle");
 	}
-	board[e.target.dataset.row][e.target.dataset.column] = e.target.className;
-	e.target.removeEventListener("click", checkPlayerLength);
-	if (player1.length > 2 || player2.length > 2) {
-		checkWinner();
-	}
 };
 
-const checkWinner = () => {
-	for (let combination of winningCombinations) {
-		if (combination.every((el) => player1.includes(el))) {
-			console.log("gracz pierwszy wygrał!");
-			boxes.forEach((box) => {
-				box.removeEventListener("click", checkPlayerLength);
-			});
-		} else if (combination.every((el) => player2.includes(el))) {
-			console.log("gracz drugi wygrał!");
-			boxes.forEach((box) => {
-				box.removeEventListener("click", checkPlayerLength);
-			});
-		}
+const mouseOutElement = (e) => {
+	if (player1.length === player2.length) {
+		e.target.classList.remove("cross");
+	} else {
+		e.target.classList.remove("circle");
 	}
 };
-
-// const checkDraw = () => {
-// 	if()
-// }
 
 boxes.forEach((box) => {
-	box.addEventListener("click", checkPlayerLength);
+	// box.addEventListener("click", checkPlayerLength);
+	box.addEventListener("mouseover", mouseOverElement);
+	box.addEventListener("mouseout", mouseOutElement);
 });
