a41177b68d5313c1b6494db916112efba36da285
Bartosz Król <barto.kro@gmail.com>
2021-03-09 23:37:43 +0100

    change functions name in script.js, create new function to simplify the code and in checkWinner function check draw possibility

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
 const boxes = Array.from(document.querySelectorAll(".board--box"));
-
 const markChoice = document.querySelector(".mark--choice");
-
 const buttons = [...document.querySelectorAll("button")];
-buttons.forEach((el) => {
-	el.addEventListener("click", (e) => {
-		player1.mark = e.target.value;
-		markChoice.classList.add("inactive");
-		if (player1.mark === "circle") {
-			player2.mark = "cross";
-		} else {
-			player2.mark = "circle";
-		}
-		console.log(player1);
-		console.log(player2);
-	});
-});
 
 const player1 = {
 	mark: null,
 	arr: [],
+	active: true,
+	wins: null,
 };
 
 const player2 = {
 	mark: null,
 	arr: [],
+	active: false,
+	wins: null,
 };
 
 const board = [
-	[, ,],
-	[, ,],
-	[, ,],
+	[null, null, null],
+	[null, null, null],
+	[null, null, null],
 ];
 
 const winningCombinations = [
@@ -44,64 +33,92 @@ const winningCombinations = [
 	[3, 5, 7],
 ];
 
+const pushBoxIntoPlayerArr = (e, player) => {
+	player.arr.push(Number(e.target.id));
+	e.target.classList.add(player.mark);
+};
+
 const checkPlayerLength = (e) => {
 	if (player1.arr.length === player2.arr.length) {
-		player1.arr.push(Number(e.target.id));
-		e.target.classList.add(player1.mark);
+		pushBoxIntoPlayerArr(e, player1);
 	} else {
-		player2.arr.push(Number(e.target.id));
-		e.target.classList.add(player2.mark);
+		pushBoxIntoPlayerArr(e, player2);
 	}
-	board[e.target.dataset.row][e.target.dataset.column] = e.target.className;
-	e.target.removeEventListener("click", checkPlayerLength);
+	board[e.target.dataset.row][e.target.dataset.column] = e.target.id;
 	if (player1.arr.length > 2 || player2.arr.length > 2) {
 		checkWinner();
 	}
-	e.target.classList.remove("cross--hover");
-	e.target.classList.remove("circle--hover");
-	e.target.removeEventListener("mouseout", mouseOutElement);
-	e.target.removeEventListener("mouseover", mouseOverElement);
+
+	e.target.classList.remove(`${player1.mark}--hover`);
+	e.target.classList.remove(`${player2.mark}--hover`);
+	removeEventListeners(e.target);
+};
+
+const removeEventListeners = (el) => {
+	el.removeEventListener("click", checkPlayerLength);
+	el.removeEventListener("mouseout", hideElementOnMouseOut);
+	el.removeEventListener("mouseover", showElementOnMouseOver);
+};
+
+const removeListenersForEachBox = () => {
+	boxes.forEach((box) => {
+		removeEventListeners(box);
+	});
 };
 
 const checkWinner = () => {
 	for (let combination of winningCombinations) {
 		if (combination.every((el) => player1.arr.includes(el))) {
 			console.log("gracz pierwszy wygrał!");
-			boxes.forEach((box) => {
-				box.removeEventListener("click", checkPlayerLength);
-				box.removeEventListener("mouseout", mouseOutElement);
-				box.removeEventListener("mouseover", mouseOverElement);
-			});
+			removeListenersForEachBox();
+			player1.wins++;
 		} else if (combination.every((el) => player2.arr.includes(el))) {
 			console.log("gracz drugi wygrał!");
-			boxes.forEach((box) => {
-				box.removeEventListener("click", checkPlayerLength);
-				box.removeEventListener("mouseout", mouseOutElement);
-				box.removeEventListener("mouseover", mouseOverElement);
-			});
+			removeListenersForEachBox();
+			player2.wins++;
+		} else if (
+			boxes.every(
+				(box) =>
+					box.classList.contains("circle") ||
+					box.classList.contains("cross")
+			)
+		) {
+			console.log("it's a draw!");
 		}
 	}
 };
 
-// // const checkDraw = () => {
-// // 	if()
-// // }
+// const checkDraw = () => {
+// 	if(board.length === 9 && board)
+// }
 
-const mouseOverElement = (e) => {
+const showElementOnMouseOver = (e) => {
 	if (player1.arr.length === player2.arr.length) {
-		e.target.classList.add("cross--hover");
+		e.target.classList.add(`${player1.mark}--hover`);
 	} else {
-		e.target.classList.add("circle--hover");
+		e.target.classList.add(`${player2.mark}--hover`);
 	}
 };
 
-const mouseOutElement = (e) => {
-	e.target.classList.remove("cross--hover");
-	e.target.classList.remove("circle--hover");
+const hideElementOnMouseOut = (e) => {
+	e.target.classList.remove(`${player1.mark}--hover`);
+	e.target.classList.remove(`${player2.mark}--hover`);
 };
 
 boxes.forEach((box) => {
 	box.addEventListener("click", checkPlayerLength);
-	box.addEventListener("mouseover", mouseOverElement);
-	box.addEventListener("mouseout", mouseOutElement);
+	box.addEventListener("mouseover", showElementOnMouseOver);
+	box.addEventListener("mouseout", hideElementOnMouseOut);
+});
+
+buttons.forEach((button) => {
+	button.addEventListener("click", (e) => {
+		player1.mark = e.target.value;
+		markChoice.classList.add("inactive");
+		if (player1.mark === "circle") {
+			player2.mark = "cross";
+		} else {
+			player2.mark = "circle";
+		}
+	});
 });
