d31c3a3bfcbeaf42177dddd6feedcf13c86579a6
Bartosz Król <barto.kro@gmail.com>
2021-03-10 21:57:29 +0100

    add reset and newGame buttons in index.html and style it in style.css. Add event listeners to both of them and create a function to resetBtn

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const buttons = [...document.querySelectorAll("button")];
 const resultPlayer1 = document.querySelector(".result--current__player1");
 const resultPlayer2 = document.querySelector(".result--current__player2");
 const latest = document.querySelector(".result--latest__results");
+const resetBtn = document.querySelector(".buttons--button__reset");
+const newGameBtn = document.querySelector(".buttons--button__newGame");
 
 const player1 = {
 	name: "player1",
 	mark: null,
 	arr: [],
-	active: true,
 	wins: null,
 	winner: null,
 };
@@ -18,12 +19,11 @@ const player2 = {
 	name: "player2",
 	mark: null,
 	arr: [],
-	active: false,
 	wins: null,
 	winner: null,
 };
 
-const board = [
+let board = [
 	[null, null, null],
 	[null, null, null],
 	[null, null, null],
@@ -40,7 +40,7 @@ const winningCombinations = [
 	[3, 5, 7],
 ];
 
-const latestResults = [];
+let latestResults = [];
 
 const pushBoxIntoPlayerArr = (e, player) => {
 	player.arr.push(Number(e.target.id));
@@ -75,18 +75,22 @@ const removeListenersForEachBox = () => {
 	});
 };
 
+const addWins = (player) => {
+	player.wins++;
+};
+
 const checkWinner = () => {
 	for (let combination of winningCombinations) {
 		if (combination.every((el) => player1.arr.includes(el))) {
 			console.log("gracz pierwszy wygrał!");
 			removeListenersForEachBox();
-			player1.wins++;
+			addWins(player1);
 			player1.winner = true;
 			resultPlayer1.textContent = `Player1: ${player1.wins}`;
 		} else if (combination.every((el) => player2.arr.includes(el))) {
 			console.log("gracz drugi wygrał!");
 			removeListenersForEachBox();
-			player2.wins++;
+			addWins(player2);
 			player2.winner = true;
 			resultPlayer2.textContent = `${player2.wins} :Player2`;
 		} else if (
@@ -141,4 +145,31 @@ buttons.forEach((button) => {
 			player2.mark = "circle";
 		}
 	});
+});
+
+resetBtn.addEventListener("click", () => {
+	board = [
+		[null, null, null],
+		[null, null, null],
+		[null, null, null],
+	];
+	boxes.forEach((box) => {
+		box.className = "board--box";
+		box.addEventListener("click", checkPlayerLength);
+		box.addEventListener("mouseover", showElementOnMouseOver);
+		box.addEventListener("mouseout", hideElementOnMouseOut);
+	});
+	player1.arr = [];
+	player1.mark = null;
+	player1.wins = 0;
+	player1.winner = false;
+	resultPlayer1.textContent = `Player1: ${player1.wins}`;
+	player2.arr = [];
+	player2.mark = null;
+	player2.wins = 0;
+	player2.winner = false;
+	resultPlayer2.textContent = `${player1.wins} :Player2 `;
+	latestResults = [];
+	latest.innerHTML = "";
+	markChoice.classList.remove("inactive");
 });
