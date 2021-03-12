eba3f78c281d56262bb6856e1db6713e55a151be
Bartosz Król <barto.kro@gmail.com>
2021-03-12 01:54:16 +0100

    create seperate function showWinningBoxes

--- script.js
+++ script.js
@@ -164,6 +164,13 @@ const removeListenersForEachBox = () => {
 	});
 };
 
+const showWinningBoxes = (player) => {
+	for (let playerNumber of player.arr) {
+		const winningBox = boxes.filter((el) => el.id == playerNumber);
+		winningBox[0].classList.add("won");
+	}
+};
+
 const setWinner = (player) => {
 	console.log(`${player.name} wygrał!`);
 	removeListenersForEachBox();
@@ -175,20 +182,12 @@ const checkWinner = () => {
 	for (let combination of winningCombinations) {
 		if (combination.every((el) => player1.arr.includes(el))) {
 			setWinner(player1);
-			for (let player of player1.arr) {
-				console.log(player);
-				const winningBox = boxes.filter((el) => el.id == player);
-				winningBox[0].classList.add("won");
-			}
+			showWinningBoxes(player1);
 			resultPlayer1.textContent = `Player1: ${player1.wins}`;
 			unlockNewGameBtn();
 		} else if (combination.every((el) => player2.arr.includes(el))) {
 			setWinner(player2);
-			for (let player of player2.arr) {
-				console.log(player);
-				const winningBox = boxes.filter((el) => el.id == player);
-				winningBox[0].classList.add("won");
-			}
+			showWinningBoxes(player2);
 			resultPlayer2.textContent = `${player2.wins} :Player2`;
 			unlockNewGameBtn();
 		}
